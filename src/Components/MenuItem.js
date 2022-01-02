bf1bae85d88990f983a0df1314386e2fe006ef67
olgaz <olypoly1@gmail.com>
2022-01-02 03:43:56 +0200

    First phase of menu app

---
+++ src/Components/MenuItem.js
@@ -0,0 +1,22 @@
+
+const MenuItem = (props) => {
+    return (
+        <li>
+            <div class="plate">
+                <img src="images/plate__french-fries.png" alt="French Fries" class="plate" />
+            </div>
+            <div class="content">
+                <p class="menu-item">French Fries with Ketchup</p>
+                <p class="price">$2.23</p>
+                <button class="in-cart">
+                    <img src="images/check.svg" alt="Check" />
+                    In Cart
+                </button>
+            </div>
+        </li>
+    );
+}
+
+
+
+export default MenuItem;
